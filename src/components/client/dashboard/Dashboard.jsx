91982067d8b60b2ccbbd171d9aa4fdbe82aca324
Samuel Albi Pulo Sibarani <samuelalbi09@gmail.com>
2024-11-06 14:42:38 +0700

    fixed

--- src/components/client/dashboard/Dashboard.jsx
+++ src/components/client/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./Dashboard.css";
+// import cardData from "./Dashboard.json";
 import cardData from "./Dashboard.json"
 
 const Dashboard = () => {
